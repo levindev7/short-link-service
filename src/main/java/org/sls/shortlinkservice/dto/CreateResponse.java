1cf22133dfa513ff662f0307e65ca79d19a34edf
levindev7 <32973916+levindev7@users.noreply.github.com>
2020-06-04 17:59:26 +0300

    Adding a domain to a variable

--- src/main/java/org/sls/shortlinkservice/dto/CreateResponse.java
+++ src/main/java/org/sls/shortlinkservice/dto/CreateResponse.java
@@ -6,10 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 @Value
 @Slf4j
 public class CreateResponse {
-    String link;
+    String token;
+    String host = "localhost:8080/";
 
-    public CreateResponse(String link) {
-        this.link = "localhost:8080/" + link;
+    /*
+    Need to get the domain using the method -
+    InetAddress address = InetAddress.getLocalHost();
+    String domain = address.getCanonicalHostName();*/
+
+    public CreateResponse(String token) {
+        this.token = host + token;
         log.info("create response");
     }
 }
